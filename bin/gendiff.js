1def7278644f430e099178f97880258ac99c4efa
Анастасия Кор <idealizm@mail.ru>
2025-06-24 13:24:19 +0300

    second step

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander'
+const program = new Command();
+
+program
+.description('Compares two configuration files and shows a difference.')
+.arguments('<filepath1> <filepath2>')
+.option('-V, --version ', 'output the version number')
+.option('-f, --format [type]', 'output format');
+
+program.parse();
