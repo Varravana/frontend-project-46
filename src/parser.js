340b00d2c7360ff427e738dc12b7e66a15a2db5a
Анастасия Кор <idealizm@mail.ru>
2025-06-27 12:14:44 +0300

    5 step

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,6 @@
 import fs from 'fs'
 import path from 'path'
 
-
 export default (a, b) => {
   const comand1 = JSON.parse(fs.readFileSync(path.resolve(a), 'utf-8'))
   const comand2 = JSON.parse(fs.readFileSync(path.resolve(b), 'utf-8'))
