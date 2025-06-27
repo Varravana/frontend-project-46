b6b24615b5c26310d149bcad3beb757f027e8544
Анастасия Кор <idealizm@mail.ru>
2025-06-27 12:06:55 +0300

    5 step

--- src/parser.js
+++ src/parser.js
@@ -1,29 +1,29 @@
-import fs from 'fs';
-import path from 'path';
-import isEqual from 'lodash';
+import fs from 'fs'
+import path from 'path'
+import isEqual from 'lodash'
 
 export default (a, b) => {
-    const comand1 = JSON.parse(fs.readFileSync(path.resolve(a), 'utf-8'));
-    const comand2 = JSON.parse(fs.readFileSync(path.resolve(b), 'utf-8'));
+  const comand1 = JSON.parse(fs.readFileSync(path.resolve(a), 'utf-8'))
+  const comand2 = JSON.parse(fs.readFileSync(path.resolve(b), 'utf-8'))
 
-    const genDiff = (obj1, obj2) => {
-        const keys = [...new Set ([...Object.keys(obj1), ...Object.keys(obj2)])].sort()
+  const genDiff = (obj1, obj2) => {
+    const keys = [...new Set ([...Object.keys(obj1), ...Object.keys(obj2)])].sort()
 
-        const diff = keys.map((key) => {
-            if(!Object.hasOwn(obj2, key)) {
-                return `- ${key}: ${obj1[key]}`
-            }
-            if (!Object.hasOwn(obj1, key)) {
-                return `+ ${key}: ${obj2[key]}`
-            }
-            if (obj1[key] !== obj2[key]) {
-                const ret = `- ${key}: ${obj1[key]}\n+ ${key}: ${obj2[key]}`;
-                return ret;
-            }
-            return `  ${key}: ${obj1[key]}`
-        })
+    const diff = keys.map((key) => {
+      if (!Object.hasOwn(obj2, key)) {
+        return `- ${key}: ${obj1[key]}`
+      }
+      if (!Object.hasOwn(obj1, key)) {
+        return `+ ${key}: ${obj2[key]}`
+      }
+      if (obj1[key] !== obj2[key]) {
+        const ret = `- ${key}: ${obj1[key]}\n+ ${key}: ${obj2[key]}`
+        return ret
+      }
+      return `  ${key}: ${obj1[key]}`
+    })
     return `{\n${diff.join('\n')}\n}`
-    }
+  }
 
-    return genDiff( comand1, comand2)
+  return genDiff(comand1, comand2)
 }
