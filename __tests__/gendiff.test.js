be8791473623fb2f9b37f416ca25e19dcacd2806
Анастасия Кор <idealizm@mail.ru>
2025-07-17 18:54:45 +0300

    17/7#3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,20 @@
 import parser from '../src/parser.js'
 import fs from 'fs'
-import gendiff from '../bin/gendiff.js'
 
-test ('parser-json-stylish', () => {
-  expect(gendiff('__fixtures__/file3.json', '__fixtures__/file4.json')).toBe(fs.readFileSync('./__fixtures__/expectedStylish.txt', "utf-8"))
+test('parser-json-stylish', () => {
+  expect(parser('__fixtures__/file3.json', '__fixtures__/file4.json')).toBe(fs.readFileSync('./__fixtures__/expectedStylish.txt', "utf-8"))
 })
+
+test ('parser-yaml-stylish', () => {
+  expect(parser('__fixtures__/file3.yaml', '__fixtures__/file4.yaml')).toBe(fs.readFileSync('./__fixtures__/expectedStylish.txt', "utf-8"))
+})
+
+test ('parser-yaml-plain', () => {
+  expect(parser('__fixtures__/file3.yaml', '__fixtures__/file4.yaml', '-f plain')).toBe(fs.readFileSync('./__fixtures__/expectedPlain.txt', "utf-8"))
+})
+
+test ('parser-json-plain', () => {
+  expect(parser('__fixtures__/file3.json', '__fixtures__/file4.json', '-f plain')).toBe(fs.readFileSync('./__fixtures__/expectedPlain.txt', "utf-8"))
+}) 
+
+
