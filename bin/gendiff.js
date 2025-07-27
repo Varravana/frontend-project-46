79586a87a92e523e03db54ffc206fc8a44233ce5
Анастасия Кор <idealizm@mail.ru>
2025-07-27 17:20:49 +0300

    aaaa

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command()
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filepath3> <filepath4>')
   .version('0.0.1', '-V, --version ', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action ((a, b, option) => {
