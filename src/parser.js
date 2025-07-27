5391e75fdb4793a8831462f11522d76d9960e747
Анастасия Кор <idealizm@mail.ru>
2025-07-27 17:31:21 +0300

    aaaa

--- src/parser.js
+++ src/parser.js
@@ -5,16 +5,16 @@ import getDiff2 from './getDiff2.js'
 import toFormat from './formatters/index.js'
 
 const getParsData = (filePath) => {
-  if(!filePath) {
-    return
-  }
-  
   const fileFormat = path.extname(filePath)
   const data = fs.readFileSync(path.resolve(filePath), 'utf-8')
   return parsers(data, fileFormat)
 }
 
 const parser = (a, b, format = 'stylish') => {
+  if(!a || !b) {
+    return
+  }
+
   const comand1 = getParsData(a)
   const comand2 = getParsData(b)
   const diff = getDiff2(comand1, comand2)
